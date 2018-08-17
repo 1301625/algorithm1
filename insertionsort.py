cf8e4062927f8dd1af0667f67a61a44df3ea9857
hoonha <sosqsz2@naver.com>
2018-08-18 01:37:06 +0900

    bubblesort0.1

---
+++ insertionsort.py
@@ -0,0 +1,28 @@
+def insertionsort(list):
+    for i in range(len(list)):
+        for j in range(len(list)):
+            while list[i] < list[j]:
+                list[i],list[j] = list[j], list[i]
+                if list[i]>list[j]:
+                    break
+    return list
+
+
+def anthor_insertion_sort(input):
+
+    for idx, valueToInsert in enumerate(input):
+        # select the hole position where number is to be inserted
+        holePosition = idx
+
+        # check if previous no. is larger than value to be inserted
+        while holePosition > 0 and input[holePosition-1] > valueToInsert:
+            input[holePosition - 1], input[holePosition] = input[holePosition], input[holePosition-1]
+            holePosition = holePosition - 1
+
+    return input
+
+if __name__ == '__main__':
+    a= [4,6,1,3,5,2]
+    b = [0,5,1,2,8,3,9,4]
+    print(insertionsort(b))
+    print(anthor_insertion_sort(a))
