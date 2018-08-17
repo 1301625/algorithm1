c1d2b8356b551fb06e05f39c81dee8f3e9f63eab
hoonha <sosqsz2@naver.com>
2018-08-17 22:19:31 +0900

    bubblesort0.1

---
+++ selectionsort.py
@@ -0,0 +1,13 @@
+def selection(list):
+    for i in range(len(list)):
+        for j in range(len(list)):
+            if list[j] > list[i]:
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+
+    return list
+
+if __name__ == '__main__':
+    a= [4,2,5,1,8,3,9]
+    print(selection(a))
