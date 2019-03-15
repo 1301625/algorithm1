6a12078ded5f3088f8c6e3ff6ebeaab4b1bea042
hoonha <sosqsz2@naver.com>
2019-03-16 01:47:52 +0900

    K번째수

---
+++ theknumber.py
@@ -0,0 +1,30 @@
+def solution(array,commands):
+    answer=[]
+    tmp = array
+    for i in commands:
+        array=array[i[0]-1:i[1]]
+        array.sort()
+        answer.append(array[i[2]-1])
+        array = tmp
+    return answer
+
+def solution2(array,commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1],commnads))
+
+
+
+
+
+array = [1,5,2,6,3,7,4]
+commnads = []
+for i in range(3):
+    tmp= []
+    i = tmp.append(int(input()))
+    j = tmp.append(int(input()))
+    k = tmp.append(int(input()))
+    commnads.append(tmp)
+
+print(solution2(array,commnads))
+
+
+
