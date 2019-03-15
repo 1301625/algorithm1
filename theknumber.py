22943be6e45e50a395ebbc43a68a423108bce393
hoonha <sosqsz2@naver.com>
2019-03-16 01:48:30 +0900

    K번째수

--- theknumber.py
+++ theknumber.py
@@ -9,7 +9,7 @@ def solution(array,commands):
     return answer
 
 def solution2(array,commands):
-    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1],commnads))
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1],commands))
 
 
 
