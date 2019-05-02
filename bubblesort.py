bd6a210b8e72fd19d08f338b7d80487de7f7ec99
hoonha <sosqsz2@naver.com>
2019-05-02 20:49:48 +0900

    버블소트 유닛테스트 추가

--- bubblesort.py
+++ bubblesort.py
@@ -1,14 +1,20 @@
+import unittest
 
-def bubblesoft(list):
+
+def bubble_sort(list):
     for i in range(len(list)):
-        for j in range(len(list)-1):
-            if list[j] > list[j+1]:
-                list[j],list[j+1] = list[j+1],list[j]
+        for j in range(len(list) - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
     return list
 
 
+class BubbleTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual([1, 2, 4, 6, 7, 8, 9], bubble_sort([6, 8, 9, 1, 4, 7, 2]))
 
 
 if __name__ == '__main__':
-    a= [6,8,9,1,4,7,2]
-    print(bubblesoft(a))
+    a = [6, 8, 9, 1, 4, 7, 2]
+    print(bubble_sort(a))
